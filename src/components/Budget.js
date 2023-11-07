a9fe4fe3e9692ede29ece62c7e27ea9e60dd139b
promahmoud <promahmoud3@gmail.com>
2023-11-07 22:12:14 +0200

    Budget Allocation
    update

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,31 @@
+import React, { useContext, useState } from "react";
+import { AppContext } from "../context/AppContext";
 
+const Budget = () => {
+  const { budget, expenses, currency } = useContext(AppContext);
+  const [newBudget, setNewBudget] = useState(budget);
+  const totalExpenses = expenses.reduce((total, item) => {
+    return (total += item.cost);
+  }, 0);
+  const handleBudgetChange = (event) => {
+    if (event.target.value < totalExpenses) {
+      alert("You cannot reduce the budget value lower than the spending");
+    } else setNewBudget(event.target.value);
+  };
+  return (
+    <div className="alert alert-secondary">
+      <label style={{ display: "inline-block" }}>
+        Budget: {currency}
+        {Budget}
+      </label>
+      <input
+        type="number"
+        step="10"
+        value={newBudget}
+        onChange={handleBudgetChange}
+      ></input>
+    </div>
+  );
+};
+
+export default Budget;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,1 +1,55 @@
+import React, { useContext } from "react";
+import { AiFillMinusCircle } from "react-icons/ai";
+import { AiFillPlusCircle } from "react-icons/ai";
+import { AppContext } from "../context/AppContext";
 
+const ExpenseItem = (props) => {
+    const { dispatch, currency } = useContext(AppContext);
+
+    const handleDeleteExpense = () => {
+        dispatch({
+            type: "DELETE_EXPENSE",
+            payload: props.id,
+        });
+    };
+
+    const increaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: "ADD_EXPENSE",
+            payload: expense,
+        });
+    };
+
+    return (
+        <tr>
+            <td>{props.name}</td>
+            <td>
+                {currency}
+                {props.cost}
+            </td>
+            <td>
+                <AiFillPlusCircle
+                    size="2.5rem"
+                    color="green"
+                    onClick={(event) => increaseAllocation(props.name)}
+                >
+                    +
+                </AiFillPlusCircle>
+            </td>
+            <td>
+                <AiFillMinusCircle
+                    size="2.5em"
+                    color="red"
+                    onClick={handleDeleteExpense}
+                >del</AiFillMinusCircle>
+            </td>
+        </tr>
+    );
+};
+
+export default ExpenseItem;
